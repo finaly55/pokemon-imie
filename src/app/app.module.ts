7e825a785754219c5b4b633088ae3f2c4b1ddd11
merouze <frederic.merouze@gmail.com>
2019-04-03 15:00:39 +0200

    [ADD] ajout du provider dans home.ts avec recuperation methode getAllPokemons() -> console.log

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,12 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PokemonApiService } from '../pokemon-api.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  constructor(private pokemonApi : PokemonApiService){
+    this.pokemonApi.getAllPokemons().subscribe((res) => {
+      console.log(res)
+    }, error => {
+      console.log(error)
+    }
+    );
+  }
 
 }
